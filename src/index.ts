effc42b1e508be02a225da24511b0e71d00fb659
pawaan <pawaan@pawaans-MacBook-Air.local>
2025-12-16 17:17:07 +0530

    Add db , added relations of prisma

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("hii there..");
+import { prisma } from "./db";
+
+async function createUser() {
+  await prisma.user.create({
+    data: {
+      username: "Harkirat",
+      password: "1232",
+      age: 24,
+      city: "Delhi",
+    },
+  });
+}
+
+createUser()
+  .then(() => {
+    console.log("User created");
+  })
+  .catch((e) => {
+    console.error("Error creating user:", e);
+  });
